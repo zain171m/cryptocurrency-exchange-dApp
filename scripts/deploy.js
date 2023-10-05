c36c021d670f24c55139bce34261ad75ff4c22b3
Muhammad Zain Nasir <zain171m@gmail.com>
2023-10-06 00:17:52 +0500

    2 Smoke_test

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,11 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
+  // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);

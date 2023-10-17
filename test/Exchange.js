683c07e2c422c66a529c431eaa9176ee84c39dbd
Muhammad Zain Nasir <zain171m@gmail.com>
2023-10-17 11:40:26 +0500

    8_Create Exchange

---
+++ test/Exchange.js
@@ -0,0 +1,32 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString() ,'ether')
+}
+
+describe('Exchange', () => {
+    let deployer, feeAccount, exchange
+
+    const feePercent = 10
+    beforeEach(async()=>{
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
+        feeAccount = accounts[1]
+
+        //Fetch Token from Blockchain
+        const Exchange = await ethers.getContractFactory('Exchange')
+        exchange = await Exchange.deploy(feeAccount.address, feePercent)
+
+    })
+
+    describe('Deployment', () => {        
+        it('tracks the fee Account', async()=> {
+            expect(await exchange.feeAccount()).to.equal(feeAccount.address)
+        }) 
+
+        it('tracks the fee Percent', async()=> {
+            expect(await exchange.feePercent()).to.equal(feePercent)
+        }) 
+    })
+})

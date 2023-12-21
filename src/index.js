dc557707f77b05d248de7d1c02201c60b821f42b
zain171m <zain171m@gmail.com>
2023-12-22 00:52:20 +0500

    Changed Repository name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-
 import {Provider} from 'react-redux'
 import store from './store/store'
 
